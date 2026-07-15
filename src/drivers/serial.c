147bdc2de957d45431b09389de742f6dd39a9538
MaraoLT <ainaras.marao@gmail.com>
2026-07-15 14:48:40 -0300

    lab02 corrected

--- src/drivers/serial.c
+++ src/drivers/serial.c
@@ -1,44 +1,84 @@
 #include <kernel/serial.h>
 #include <kernel/panic.h>
+#include <kernel/mm.h>
+#include <kernel/trap.h>
+#include <arch/csr.h>
+#include <arch/spinlock.h>
+#include <arch/plic.h>
+
+#define UART_REG(r) ((volatile u8*)(SERIAL_BASE + (r)))
+#define SERIAL_BUF_SIZE 1024
+
+static char serial_buf[SERIAL_BUF_SIZE];
+static size_t serial_head = 0;
+static size_t serial_tail = 0;
+static struct spinlock serial_lock;
 
 void serial_init()
 {
-	/* not implemented */
-	BUG();
+    *UART_REG(SERIAL_IER) = 0x00;
+    *UART_REG(SERIAL_LCR) = 0x80;
+    *UART_REG(0) = 0x01;
+    *UART_REG(1) = 0x00;
+    *UART_REG(SERIAL_LCR) = 0x03;
+    *UART_REG(SERIAL_FCR) = 0x07;
+    *UART_REG(SERIAL_IER) = SERIAL_IER_ERBFI;
 }
 
 void serial_irq_enable()
 {
-	/* not implemented */
-	BUG();
+    plic_irq_set_priority(10, 1);
+    plic_hart_set_threshold(0, 0);
+    plic_hart_enable_irq(0, 10);
+
+    csr_set(CSR_SIE, (1UL << 9)); // enable external interrupt
 }
 
 void serial_irq_disable()
 {
-	/* not implemented */
-	BUG();
+    csr_clear(CSR_SIE, (1UL << 9)); // disable external interrupt
 }
 
 void serial_irq()
 {
-	/* not implemented */
-	BUG();
+    spin_lock(&serial_lock);
+
+    while (*UART_REG(5) & 0x01) {
+        char c = *UART_REG(0);
+        size_t next_head = (serial_head + 1) % SERIAL_BUF_SIZE;
+        if (next_head != serial_tail) {
+            serial_buf[serial_head] = c;
+            serial_head = next_head;
+        }
+    }
+    spin_unlock(&serial_lock);
 }
 
 size_t serial_read(char *buf)
 {
-	/* not implemented */
-	BUG();
+    size_t count = 0;
+    u64 flags = hart_irq_save();
+    spin_lock(&serial_lock);
+
+    while (serial_tail != serial_head) {
+        buf[count++] = serial_buf[serial_tail];
+        serial_tail = (serial_tail + 1) % SERIAL_BUF_SIZE;
+    }
+    spin_unlock(&serial_lock);
+    hart_irq_restore(flags);
+
+    return count;
 }
 
 void serial_puts(char *str)
 {
-	/* not implemented */
-	BUG();
+    while (*str) {
+        serial_putc(*str++);
+    }
 }
 
 void serial_putc(char c)
 {
-	/* not implemented */
-	BUG();
+    while (!(*UART_REG(5) & 0x20));
+    *UART_REG(0) = (u8)c;
 }
--- src/trap.c
+++ src/trap.c
@@ -1,53 +1,92 @@
 #include <kernel/trap.h>
 #include <kernel/panic.h>
+#include <kernel/printf.h>
+#include <kernel/serial.h>
+#include <arch/csr.h>
+#include <arch/timer.h>
+#include <arch/plic.h>
 
 /* defined in src/trap_entry.S */
 extern void trap_entry();
 
 void handle_irq()
 {
-	/* not implemented */
-	BUG();
+    u64 scause = csr_read(CSR_SCAUSE);
+    u64 code = scause & ~(1UL << 63);
+
+    if (code == 5) {
+        timer_irq();
+    } else if (code == 9) {
+        u32 irq = plic_hart_claim_irq(0);
+
+        if (irq == 10) {
+            serial_irq();
+        }
+        if (irq != 0) {
+            plic_hart_complete_irq(0, irq);
+        }
+    } else {
+        warn("Unknown interrupt occurred! scause: 0x%lx, code: %lu\n", scause, code);
+    }
 }
 
 void handle_exception()
 {
-	/* not implemented */
-	BUG();
+    u64 scause = csr_read(CSR_SCAUSE);
+    u64 stval = csr_read(CSR_STVAL);
+    u64 sepc = csr_read(CSR_SEPC);
+
+    u64 code = scause & ~(1UL << 63);
+
+    error("Exception occurred! scause: 0x%lx, stval: 0x%lx, sepc: 0x%lx, code: %lu\n", scause, stval, sepc, code);
+
+    BUG();
 }
 
 void trap_setup()
 {
-	/* not implemented */
-	BUG();
+	csr_write(CSR_STVEC, (u64)trap_entry);
 }
 
 void handle_trap()
 {
-	/* not implemented */
-	BUG();
+	u64 scause = csr_read(CSR_SCAUSE);
+    int is_interrupt = scause >> 63;
+
+    if (is_interrupt) {
+        handle_irq();
+    } else {
+        handle_exception();
+    }
 }
 
 void hart_irq_enable()
 {
-	/* not implemented */
-	BUG();
+    u64 sstatus = csr_read(CSR_SSTATUS);
+    csr_write(CSR_SSTATUS, sstatus | (1UL << 1));
 }
 
 u64 hart_irq_save()
 {
-	/* not implemented */
-	BUG();
+    u64 sstatus = csr_read(CSR_SSTATUS);
+    csr_write(CSR_SSTATUS, sstatus & ~(1UL << 1));
+
+    return sstatus;
 }
 
 void hart_irq_restore(u64 flags)
 {
-	/* not implemented */
-	BUG();
+    u64 sstatus = csr_read(CSR_SSTATUS);
+    if (flags & (1UL << 1)) {
+        sstatus |= (1UL << 1);
+    } else {
+        sstatus &= ~(1UL << 1);
+    }
+    csr_write(CSR_SSTATUS, sstatus);
 }
 
 void hart_irq_disable()
 {
-	/* not implemented */
-	BUG();
+    u64 sstatus = csr_read(CSR_SSTATUS);
+    csr_write(CSR_SSTATUS, sstatus & ~(1UL << 1));
 }
